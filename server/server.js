db4867a2d58ead5a59d39583e8c75bf835b97127
SergeyZimakov <zimakovs93@gmail.com>
2021-01-30 20:17:45 +0200

    complete

--- server/server.js
+++ server/server.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const mysql = require('mysql');
+const webSocketsHandler = require('./utils/webSocketsHandler');
 
-const server = express();
+const app = express()
+const server = require('http').createServer(app);
+webSocketsHandler.initialize(server);
 
-server.listen(8080, () => console.log('Server is running on port 8080'));
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(session({
+	secret: "somesecret",
+	resave: true,
+	saveUninitialized: true
+}));
+
+// app.use(express.static(require('path').join(__dirname, '../upload')));
+app.use(express.static('upload'));
+app.use('/api', require('../server/apiRouter'));
+
+const port = 8080;
+// app.listen(port, () => console.log(`Server is running on port ${port}`))
+server.listen(port, () => console.log(`Server is running on port ${port}`))
